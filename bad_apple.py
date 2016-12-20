ee0a84c3bac9b11319e172d554e588c05a417275
jackson97 <jake.jackson97@yahoo.co.uk>
2016-12-20 09:19:13 +0000

    Update bad_apple.py

--- bad_apple.py
+++ bad_apple.py
@@ -8,41 +8,58 @@ import string
 from Tkinter import *
 import tkFileDialog
 import Tkconstants
-
+ 
 root = Tk()
-guid_pattern = re.compile('^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$')
-
+ 
 def mount_disk():
+    global GUID
+    global GUIDstr
     loop = True
     print('Please select the .dmg file you would like to decrypt.')
     disk_to_mount = tkFileDialog.askopenfilename()
     print (disk_to_mount)
     os.system('hdiutil attach -nomount %s' % (disk_to_mount))
-    mount_command = subprocess.Popen('diskutil cs list', shell=True, stdout=subprocess.PIPE).stdout
-    mount_output = mount_command.read()
-    for line in mount_output:
-        if 'Logical Volume ' in mount_output:
-            GUID = line.split('Logical Volume',1)
-            if (guid_pattern.match(GUID)):
-                start_cracking = raw_input('Do you want to begin cracking? [Y/N]: ')
-                if start_cracking in ('y' or 'Y'):
-                    password_attempt()
+    chkdisk_command = subprocess.Popen('diskutil cs list', shell=True, stdout=subprocess.PIPE).stdout
+    chkdisk_output = chkdisk_command.read()
+    old_stdout = sys.stdout
+    with open('temp.txt', 'w') as t:
+        sys.stdout = t
+        print chkdisk_output
+        sys.stdout = old_stdout
+    with open('temp.txt', 'r') as log:
+        guid_pattern = re.compile('^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$')
+        while (loop == True):
+            for line in log:
+                if 'Logical Volume ' in line:
+                    GUID = line.split('Logical Volume ')[1]
+                    if (guid_pattern.match(GUID)):
+                        f = open( 'GUIDtemp.txt', 'rw' )
+                        f.write(GUID)
+                        GUIDstr = f.read()
+                        f.close
+                        print('Image mounted and locked partition found. GUID: ' + GUIDstr)
+                        start_cracking = raw_input('Do you want to begin cracking? [Y/N]: ')
+                        if start_cracking in ('y' or 'Y'):
+                            password_attempt()
+                        else:
+                            print('Thank you for using Bad_Apple')
+                            quit()
+                    else:
+                        loop = True
                 else:
-                    print('Thank you for using Bad_Apple')
-                    quit()
-            else:
-                loop = True
-        else:
-            loop=True
-            
+                    loop = True
+             
 def password_attempt():
-    with open('sevenDigitDict.txt') as f:
+    print("getting this far!!!")
+    with open('numCodeDict.txt', 'r') as f:
         for password in f:
-            unlock_command = subprocess.Popen('diskutil cs unlockVolume %s -passphrase %s &> /dev/null' % (GUID, password), shell=True, stdout=subprocess.PIPE).stdout
+            unlock_command = subprocess.Popen('diskutil cs unlockVolume %s -passphrase %s' % (GUIDstr, password), shell=True, stdout=subprocess.PIPE).stdout
+            #with open("log.txt", 'a') as fo:
+                #fo.write(unlock_command.read())
             unlock_output = unlock_command.read()
             if 'unlocked' in unlock_output:
                 print('Unlocked with the following password: %s' % (password))
-                allocated_disk = int(re.search(r'\d+', output).group())
+                allocated_disk = int(re.search(r'\d+', unlock_output).group())
                 image_partition = raw_input('Do you want to acquire the unlocked partition? [Y/N]: ')
                 if image_partition in ('y' or 'Y'):
                     print('Please select the desired ouput directory.')
@@ -54,24 +71,10 @@ def password_attempt():
                 else:
                     print('Thank you for using Bad_Apple')
                     quit()
-
+ 
 os.system('clear')
 print('Welcome to Bad_Apple\n')
 time.sleep(0.5)
-
-global GUID
+ 
+global loop
 mount_disk()
-
-while(loop == True):
-    GUID = raw_input('Please enter the GUID of the drive you wish to crack: ')
-    print('Type exit/Exit to quit)')
-    if (GUID == 'exit' or GUID == 'Exit'):
-        break
-    else:
-        if (guid_pattern.match(GUID)):
-            password_attempt()
-        else:
-            print "'" + GUID + "' is not a GUID"
-            time.sleep(1)
-            os.system('clear')
-            loop = True
